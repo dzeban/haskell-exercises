27efa519e75d118a95e02e5e5749febb1e534b7f
Alex Dzyoba <alex@dzyoba.com>
2016-02-19 19:10:51 +0300

    CIS-194: HW04:  foldTree solution

--- hw4/HW04.hs
+++ hw4/HW04.hs
@@ -15,6 +15,27 @@ fun2 n
     | even n    = n + fun2 (n `div` 2)
     | otherwise = fun2 (3 * n + 1)
 
+data Tree a = Leaf | Node Integer (Tree a) a (Tree a)
+    deriving (Show, Eq)
+
+foldTree :: [a] -> Tree a
+foldTree xs = foldr insert Leaf xs
+
+insert :: a -> Tree a -> Tree a
+insert a Leaf = Node 1 Leaf a Leaf
+insert a (Node i left n right) =
+    if height left <= height right then
+        Node (height newLeft + 1) newLeft n right
+    else
+        Node (height newRight + 1) left n newRight
+    where
+        newLeft = insert a left
+        newRight = insert a right
+
+height :: Tree a -> Integer
+height Leaf = 0
+height (Node i _ _ _) = i
+
 xor' :: [Bool] -> Bool
 xor' xs = odd . length $ filter (==True) xs
 
