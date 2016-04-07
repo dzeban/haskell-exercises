96aae1e1b04e154c0d44dd90e16bffa2a02e48c4
Alex Dzyoba <alex@dzyoba.com>
2016-04-07 18:36:10 +0300

    Add O(n) fibonacci implementation

--- hw6/Fibonacci.hs
+++ hw6/Fibonacci.hs
@@ -8,3 +8,9 @@ fib n = fib (n-1) + fib (n-2)
 
 fibs1 :: [Integer]
 fibs1 = map fib [0..]
+
+fibs2 :: [Integer]
+fibs2 = map fst $ iterate fibOp (0, 1)
+
+fibOp :: Num a => (a, a) -> (a, a)
+fibOp (a, b) = (b, a+b)
